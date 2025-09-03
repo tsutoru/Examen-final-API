016e81ee9d209a2aac38df8d20c1e44ca37b89cf
harena <tsutoru24@gmail.com>
2025-09-03 09:26:37 +0300

    first exercice get/health return ok

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/ping")
+@app.get("/health")
 async def root():
-    return {"message": "pong"}
+    return "ok"
+
+@app.post("/phones")
+async def creation():
+    return "null"
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+@app.get("/phones/{Id}")
+async def say_hello(Id: int):
+    return {"message": f"Hello {Id}"}
